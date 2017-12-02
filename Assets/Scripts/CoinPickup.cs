559da69a6d7c4ad773a0ae67e60315ab74998362
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-12-02 11:44:13 +0200

    Coin pickup -> Head growth

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public Transform head;
+
+    public float headGrowthPerPickup;
+    
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Coin"))
+        {
+            head.localScale += Vector3.one * headGrowthPerPickup;
+            
+            Destroy(collider.gameObject);
+        }
+    }
+}

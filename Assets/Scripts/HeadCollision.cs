27a02c23ae7d2c7e380e098bdfb95537b11fdcc4
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-12-02 11:34:42 +0200

    Head collision -> restart!

---
+++ Assets/Scripts/HeadCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeadCollision : MonoBehaviour
+{
+    public GameObject head;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (
+            collision.collider.CompareTag("Ground") &&
+            collision.otherCollider.CompareTag("PlayerHead")
+        )
+        {
+            head.SetActive(false);
+            Invoke("Restart", 2.0f);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}

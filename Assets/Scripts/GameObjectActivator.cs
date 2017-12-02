20a05d65ca06eeeebb5f2e693e0cb66af756e18a
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-12-02 19:36:57 +0200

    object activator

---
+++ Assets/Scripts/GameObjectActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectActivator : MonoBehaviour
+{
+	public GameObject[] objectsToActivate;
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (
+			collider.CompareTag("Player")	 ||
+			collider.CompareTag("PlayerHead")
+		)
+		{
+			foreach (var obj in objectsToActivate) {
+				obj.SetActive(true);
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,26 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.Q))
+		if (Input.GetKeyDown(KeyCode.Z))
 		{
-			SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
+			SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));			
 		}
+	
+		// CHEAT
+		
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			transform.position = new Vector3(156f, 9.3f, 0f);
+		}
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			transform.position = new Vector3(77.1f, 70.2f, 0f);
+		}
+		if (Input.GetKeyDown(KeyCode.V))
+		{			
+			transform.position = new Vector3(-53.6f, 70.3f, 0f);
+		}
+		
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			characterMovement.Jump();

0a21e78b086795ae308f56561efd0fe3503118a8
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-12-02 16:12:07 +0200

    lift

---
+++ Assets/Scripts/Lift.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	public float liftingVelocity;
+	public float mass;
+	
+	public float liftDelay;
+
+	private bool activated = false;
+	
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (activated)
+		{
+			rb.velocity = Vector2.up * liftingVelocity;			
+		}		
+	}
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (
+			collision.collider.CompareTag("Player") ||
+			collision.collider.CompareTag("PlayerHead")
+		)
+		{
+			Invoke("Activate", liftDelay);
+		}
+		
+	}
+
+	private void Activate()
+	{
+		rb.bodyType = RigidbodyType2D.Dynamic;
+		rb.mass = mass;
+		activated = true;
+	}
+	
+}

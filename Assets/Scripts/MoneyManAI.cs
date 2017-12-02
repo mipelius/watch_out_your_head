cf93add2ba468aa5992c315cfa1cf204eb90718f
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-12-02 13:06:57 +0200

    enemy: moneyman

--- Assets/Scripts/MoneyManAI.cs
+++ Assets/Scripts/MoneyManAI.cs
@@ -2,15 +2,37 @@
 
 public class MoneyManAI : MonoBehaviour
 {
-	public float runningSpeed;
-	
-	
-	
-	void Start () {
+	public Transform target;
+
+	public float jumpInterval = 1.0f;
+
+	public float jumpProbability;
 		
+	private CharacterMovement movement;
+
+	private float lastJumpTimestamp;
+	
+	void Awake()
+	{
+		movement = GetComponent<CharacterMovement>();
+		lastJumpTimestamp = Time.time;
 	}
-	
-	void Update () {
-		
+
+	void FixedUpdate()
+	{
+		movement.horizontalMovement = Mathf.Sign(target.position.x - transform.position.x);		
+	}
+
+	void Update()
+	{
+		if (Time.time - lastJumpTimestamp > jumpInterval)
+		{
+			lastJumpTimestamp = Time.time;
+			
+			if (Random.Range(0f, 1f) < jumpProbability)
+			{
+				movement.Jump();
+			}
+		}
 	}
 }
